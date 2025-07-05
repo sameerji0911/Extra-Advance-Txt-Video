91d9acd595abeffd8fda90e4c444cf7764732a5a
sameerji0911 <shammaarbaz@gmail.com>
2025-07-05 17:47:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ bot = Client(
 cookies_file_path= "youtube_cookies.txt"
 
 
-auth_users = [7212452634,7341059064,5817712634,7958016772]
+auth_users = [7431004177,]
 
 # Command to authorize a user
 @bot.on_message(filters.command("auth") & filters.private)
